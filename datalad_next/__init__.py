907478e4d4423d00c8bc5b857152e5b3c577197e
Michael Hanke <michael.hanke@gmail.com>
2022-03-30 15:13:33 +0200

    Adjust extension template

--- datalad_next/__init__.py
+++ datalad_next/__init__.py
@@ -1,27 +1,23 @@
-"""DataLad demo extension"""
+"""DataLad NEXT extension"""
 
 __docformat__ = 'restructuredtext'
 
 import logging
-lgr = logging.getLogger('datalad.helloworld')
+lgr = logging.getLogger('datalad.next')
 
 # Defines a datalad command suite.
 # This variable must be bound as a setuptools entrypoint
 # to be found by datalad
 command_suite = (
     # description of the command suite, displayed in cmdline help
-    "Demo DataLad command suite",
+    "What is next in DataLad",
     [
         # specification of a command, any number of commands can be defined
         (
             # importable module that contains the command implementation
-            'datalad_helloworld.hello_cmd',
+            'datalad_next.credentials',
             # name of the command class implementation in above module
-            'HelloWorld',
-            # optional name of the command in the cmdline API
-            'hello-cmd',
-            # optional name of the command in the Python API
-            'hello_cmd'
+            'Credentials',
         ),
     ]
 )
