674317202a6d8009df0f003dde58fa65f84c60b0
Christian Mönch <christian.moench@web.de>
2022-04-08 08:47:31 +0200

    update comments

--- datalad_next/patches/push_to_export_remote.py
+++ datalad_next/patches/push_to_export_remote.py
@@ -20,19 +20,37 @@ __docformat__ = "restructuredtext"
 lgr = logging.getLogger('datalad_next.push_to_export_remote')
 
 
-def _is_export_remote_webdav(repo: AnnexRepo,
-                             target: str,
-                             ) -> bool:
-    """Check whether a remote has exporttree set to "yes"
+def _is_export_remote(repo: AnnexRepo,
+                      target: str,
+                      ) -> bool:
+    """Check if remote "target" exists and has exporttree set to "yes"
+
     :param repo: the repository that contains the remote
     :param target: name of the remote that should be checked
-    :param res_kwargs: preset kwargs for the result
     :return: True if git annex reports exporttree is yes, else False
     """
     remote_info = [
         record
         for record in repo.get_special_remotes().values()
-        if record.get("name") == target and record.get("type") == "webdav"]
+        if record.get("name") == target]
+    if len(remote_info) == 1:
+        return remote_info[0].get("exporttree") == "yes"
+    return False
+
+
+def _is_webdav_remote(repo: AnnexRepo,
+                      target: str,
+                      ) -> bool:
+    """Check if remote "target" exists and has type set to "webdav"
+
+    :param repo: the repository that contains the remote
+    :param target: name of the remote that should be checked
+    :return: True if git annex reports exporttree is yes, else False
+    """
+    remote_info = [
+        record
+        for record in repo.get_special_remotes().values()
+        if record.get("name") == target]
     if len(remote_info) == 1:
         return remote_info[0].get("exporttree") == "yes"
     return False
@@ -49,12 +67,29 @@ def _transfer_data(repo: AnnexRepo,
                    got_path_arg: bool
                    ) -> Generator:
 
-    if _is_export_remote_webdav(repo, target):
-        yield from ExportToWEBDAV()(
-            dataset=ds,
-            to=target,
-            url=None,
-            mode="auto"
+    if _is_export_remote(repo, target):
+        # TODO:
+        #  - check for configuration entries, e.g. what to export
+        #  - check for all kind of things that are checked in push._push_data
+        #  - proper error handling
+        #  - connect with credential handling
+
+        lgr.debug("Exporting HEAD to a remote with exporttree == yes")
+
+        if ds.config.getbool('remote.{}'.format(target), 'annex-ignore', False):
+            lgr.debug(
+                "Target '%s' is set to annex-ignore, exclude from data-export.",
+                target,
+            )
+            return
+
+        res_kwargs['target'] = target
+
+        repo.call_git(["annex", "export", "HEAD", "--to", target])
+        yield dict(
+            res_kwargs,
+            action='export',
+            status='ok',
         )
     else:
         yield from push._push_data(
