cafd3d3280c2d74a0dc75a1af4539d49170ff3e3
Caterina Trainito <caterina.trainito@gmail.com>
2022-07-05 01:33:07 +0200

    set up parametrized tests to cover combinations of datalad tree options

--- datalad_next/tests/test_tree.py
+++ datalad_next/tests/test_tree.py
@@ -1,29 +1,29 @@
 import os
 
+import pytest
 from datalad.tests.utils_pytest import (
     assert_in,
     assert_not_in,
     assert_raises,
     eq_,
     neq_,
-    patch_config,
-    with_tree,
+    with_tree, assert_str_equal,
 )
+from datalad.utils import rmtemp
+
 from ..tree import Tree, Walk
 
 """
 Tests for datalad tree.
-TODO: create fixture for precomputing tree output for a given set
-of parameters, so it can be reused in multiple tests.
 """
 
-# directory layout to be tested that will be created as temp dir.
-# directory index is equals to the count of its subdirectories
-# and the (count-1) of files contained underneath it.
-# TODO: generate programmatically instead of hardcoding
-# (though it's easier to visualize if hardcoded)
+# directory layout to be tested that will be created as temp dir
 _temp_dir_tree = {
     "root": {
+        ".dir3": {
+            "dir3_file0": 'tempfile',
+            ".dir3_file1": 'tempfile',
+        },
         "dir0": {},  # empty dir
         "dir1": {
             "dir1_file0": 'tempfile',
@@ -38,22 +38,67 @@ _temp_dir_tree = {
                 "dir2_dir2_file1": 'tempfile',
             },
             "dir2_file0": 'tempfile',
-            "dir2_file1":'tempfile'
+            "dir2_file1": 'tempfile',
         },
+        ".file2": 'tempfile',
         "file0": 'tempfile',
         "file1": 'tempfile',
     }
 }
 
 
-@with_tree(_temp_dir_tree)
-def test_build_tree_dirs_only(path=None):
-    root = os.path.join(path, 'root')
-    walk = Walk(root, max_depth=3, include_files=False)
-    walk.build_tree()
-    actual = walk.get_tree()
+@pytest.fixture(scope="module")
+def path():
+    """
+    Create a temporary directory tree once for the whole module,
+    to be used as test data for all tests.
+    This is a shim for the 'with_tree' decorator so it can be used
+    as module-scoped pytest fixture.
+    """
+    # function to be decorated by 'with_tree'
+    # just return the argument (will be the created temp path)
+    identity_func = lambda d: d
 
-    expected = f"""{root}
+    # give an informative name to the lambda function, since
+    # it will be included in the name of the temp dir
+    identity_func.__name__ = "test_tree"
+
+    # call the 'with_tree' decorator to return the path
+    # of the created temp dir root, without deleting it
+    temp_dir_root = with_tree(_temp_dir_tree, delete=False)(identity_func)()
+    print(f"created temp dir at {temp_dir_root}")
+    yield temp_dir_root
+    rmtemp(temp_dir_root)  # this duplicates 'with_tree' code
+    print(f"deleted temp dir at {temp_dir_root}")
+
+
+def format_param_ids(val):
+    """Helper to format pytest parameter IDs.
+    If the parameter is a string containing newlines, we assume it
+    is the parameter 'expected' (expected output of tree), and just
+    give it a fixed ID."""
+    if isinstance(val, str) and "\n" in val:
+        return "expected"
+
+
+param_matrix = [
+    # (2 levels per param) ** (3 params) = 8 combinations + 8 expected results
+    # column names: depth, include_files, include_hidden, expected
+    [
+        1,
+        False,
+        False,
+        """
+├── dir0
+├── dir1
+└── dir2
+"""
+    ],
+    [
+        3,
+        False,
+        False,
+        """
 ├── dir0
 ├── dir1
 └── dir2
@@ -61,17 +106,24 @@ def test_build_tree_dirs_only(path=None):
     ├── dir2_dir1
     └── dir2_dir2
 """
-    eq_(expected, actual)
-
-
-@with_tree(_temp_dir_tree)
-def test_build_tree_including_files(path=None):
-    root = os.path.join(path, 'root')
-    walk = Walk(root, max_depth=3, include_files=True)
-    walk.build_tree()
-    actual = walk.get_tree()
-
-    expected = f"""{root}
+    ],
+    [
+        1,
+        True,
+        False,
+        """
+├── file0
+├── file1
+├── dir0
+├── dir1
+└── dir2
+"""
+    ],
+    [
+        3,
+        True,
+        False,
+        """
 ├── file0
 ├── file1
 ├── dir0
@@ -87,5 +139,86 @@ def test_build_tree_including_files(path=None):
         ├── dir2_dir2_file0
         └── dir2_dir2_file1
 """
-    eq_(expected, actual)
+    ],
+    [
+        1,
+        True,
+        True,
+        """
+├── .file2
+├── file0
+├── file1
+├── .dir3
+├── dir0
+├── dir1
+└── dir2
+"""
+    ],
+    [
+        3,
+        True,
+        True,
+        """
+├── .file2
+├── file0
+├── file1
+├── .dir3
+|   ├── .dir3_file1
+|   └── dir3_file0
+├── dir0
+├── dir1
+|   └── dir1_file0
+└── dir2
+    ├── dir2_file0
+    ├── dir2_file1
+    ├── dir2_dir0
+    ├── dir2_dir1
+    |   └── dir2_dir1_file0
+    └── dir2_dir2
+        ├── dir2_dir2_file0
+        └── dir2_dir2_file1
+"""
+    ],
+    [
+        1,
+        False,
+        True,
+        """
+├── .dir3
+├── dir0
+├── dir1
+└── dir2
+"""
+    ],
+    [
+        3,
+        False,
+        True,
+        """
+├── .dir3
+├── dir0
+├── dir1
+└── dir2
+    ├── dir2_dir0
+    ├── dir2_dir1
+    └── dir2_dir2
+"""
+    ]
+]
 
+
+@pytest.mark.parametrize(
+    ["depth", "include_files", "include_hidden", "expected"],
+    param_matrix, ids=format_param_ids
+)
+def test_print_tree_with_params(
+    path, depth, include_files, include_hidden, expected
+):
+    root = os.path.join(path, 'root')
+    walk = Walk(
+        root, max_depth=depth,
+        include_files=include_files, include_hidden=include_hidden)
+    walk.build_tree()
+    actual_res = walk.get_tree()
+    expected_res = root + expected
+    assert_str_equal(expected_res, actual_res)
