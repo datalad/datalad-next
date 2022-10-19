bbb0ae1d03ceacdaea304da8dcaec4dc42cba0f1
Michael Hanke <michael.hanke@gmail.com>
2022-10-19 11:14:08 +0200

    Move tests to be more pytest native
    Stop using assert_equal() and assert_raises()

--- datalad_next/constraints/tests/test_constraints.py
+++ datalad_next/constraints/tests/test_constraints.py
@@ -1,8 +1,4 @@
-from datalad.tests.utils_pytest import (
-    assert_equal,
-    assert_raises,
-)
-
+import pytest
 
 from ..api import (
     Constraints,
@@ -27,196 +23,237 @@ from ..utils import _type_str
 def test_int():
     c = EnsureInt()
     # this should always work
-    assert_equal(c(7), 7)
-    assert_equal(c(7.0), 7)
-    assert_equal(c('7'), 7)
-    assert_equal(c([7, 3]), [7, 3])
+    assert c(7) == 7
+    assert c(7.0) == 7
+    assert c('7') == 7
+    assert c([7, 3]) == [7, 3]
     # this should always fail
-    assert_raises(ValueError, lambda: c('fail'))
-    assert_raises(ValueError, lambda: c([3, 'fail']))
+    with pytest.raises(ValueError):
+        c('fail')
+    with pytest.raises(ValueError):
+        c([3, 'fail'])
     # this will also fail
-    assert_raises(ValueError, lambda: c('17.0'))
-    assert_equal(c.short_description(), 'int')
+    with pytest.raises(ValueError):
+        c('17.0')
+    assert c.short_description() == 'int'
 
 
 def test_float():
     c = EnsureFloat()
     # this should always work
-    assert_equal(c(7.0), 7.0)
-    assert_equal(c(7), 7.0)
-    assert_equal(c('7'), 7.0)
-    assert_equal(c([7.0, '3.0']), [7.0, 3.0])
+    assert c(7.0) == 7.0
+    assert c(7) == 7.0
+    assert c('7') == 7.0
+    assert c([7.0, '3.0']) == [7.0, 3.0]
     # this should always fail
-    assert_raises(ValueError, lambda: c('fail'))
-    assert_raises(ValueError, lambda: c([3.0, 'fail']))
+    with pytest.raises(ValueError):
+        c('fail')
+    with pytest.raises(ValueError):
+        c([3.0, 'fail'])
 
 
 def test_bool():
     c = EnsureBool()
     # this should always work
-    assert_equal(c(True), True)
-    assert_equal(c(False), False)
+    assert c(True) is True
+    assert c(False) is False
     # all that resuls in True
-    assert_equal(c('True'), True)
-    assert_equal(c('true'), True)
-    assert_equal(c('1'), True)
-    assert_equal(c('yes'), True)
-    assert_equal(c('on'), True)
-    assert_equal(c('enable'), True)
+    assert c('True') is True
+    assert c('true') is True
+    assert c('1') is True
+    assert c('yes') is True
+    assert c('on') is True
+    assert c('enable') is True
     # all that resuls in False
-    assert_equal(c('false'), False)
-    assert_equal(c('False'), False)
-    assert_equal(c('0'), False)
-    assert_equal(c('no'), False)
-    assert_equal(c('off'), False)
-    assert_equal(c('disable'), False)
+    assert c('false') is False
+    assert c('False') is False
+    assert c('0') is False
+    assert c('no') is False
+    assert c('off') is False
+    assert c('disable') is False
     # this should always fail
-    assert_raises(ValueError, c, 0)
-    assert_raises(ValueError, c, 1)
+    with pytest.raises(ValueError):
+        c(0)
+    with pytest.raises(ValueError):
+        c(1)
 
 
 def test_str():
     c = EnsureStr()
     # this should always work
-    assert_equal(c('hello'), 'hello')
-    assert_equal(c('7.0'), '7.0')
+    assert c('hello') == 'hello'
+    assert c('7.0') == '7.0'
     # this should always fail
-    assert_raises(ValueError, lambda: c(['ab']))
-    assert_raises(ValueError, lambda: c(['a', 'b']))
-    assert_raises(ValueError, lambda: c(('a', 'b')))
+    with pytest.raises(ValueError):
+        c(['ab'])
+    with pytest.raises(ValueError):
+        c(['a', 'b'])
+    with pytest.raises(ValueError):
+        c(('a', 'b'))
     # no automatic conversion attempted
-    assert_raises(ValueError, lambda: c(7.0))
-    assert_equal(c.short_description(), 'str')
+    with pytest.raises(ValueError):
+        c(7.0)
+    assert c.short_description() == 'str'
+
 
 def test_str_min_len():
     c = EnsureStr(min_len=1)
-    assert_equal(c('hello'), 'hello')
-    assert_equal(c('h'), 'h')
-    assert_raises(ValueError, c, '')
+    assert c('hello') == 'hello'
+    assert c('h') == 'h'
+    with pytest.raises(ValueError):
+        c('')
 
     c = EnsureStr(min_len=2)
-    assert_equal(c('hello'), 'hello')
-    assert_raises(ValueError, c, 'h')
+    assert c('hello') == 'hello'
+    with pytest.raises(ValueError):
+        c('h')
 
 
 def test_none():
     c = EnsureNone()
     # this should always work
-    assert_equal(c(None), None)
+    assert c(None) is None
     # this should always fail
-    assert_raises(ValueError, lambda: c('None'))
-    assert_raises(ValueError, lambda: c([]))
+    with pytest.raises(ValueError):
+        c('None')
+    with pytest.raises(ValueError):
+        c([])
 
 
 def test_callable():
     c = EnsureCallable()
     # this should always work
-    assert_equal(c(range), range)
-    assert_raises(ValueError, c, 'range')
+    assert c(range) == range
+    with pytest.raises(ValueError):
+        c('range')
 
 
 def test_choice():
     c = EnsureChoice('choice1', 'choice2', None)
     # this should always work
-    assert_equal(c('choice1'), 'choice1')
-    assert_equal(c(None), None)
+    assert c('choice1') == 'choice1'
+    assert c(None) is None
     # this should always fail
-    assert_raises(ValueError, lambda: c('fail'))
-    assert_raises(ValueError, lambda: c('None'))
+    with pytest.raises(ValueError):
+        c('fail')
+    with pytest.raises(ValueError):
+        c('None')
 
 
 def test_keychoice():
     c = EnsureKeyChoice(key='some', values=('choice1', 'choice2', None))
-    assert_equal(c({'some': 'choice1'}), {'some': 'choice1'})
-    assert_equal(c({'some': None}), {'some': None})
-    assert_equal(c({'some': None, 'ign': 'ore'}), {'some': None, 'ign': 'ore'})
-    assert_raises(ValueError, c, 'fail')
-    assert_raises(ValueError, c, 'None')
-    assert_raises(ValueError, c, {'nope': 'None'})
-    assert_raises(ValueError, c, {'some': 'None'})
-    assert_raises(ValueError, c, {'some': ('a', 'b')})
+    assert c({'some': 'choice1'}) == {'some': 'choice1'}
+    assert c({'some': None}) == {'some': None}
+    assert c({'some': None, 'ign': 'ore'}) == {'some': None, 'ign': 'ore'}
+    with pytest.raises(ValueError):
+        c('fail')
+    with pytest.raises(ValueError):
+        c('None')
+    with pytest.raises(ValueError):
+        c({'nope': 'None'})
+    with pytest.raises(ValueError):
+        c({'some': 'None'})
+    with pytest.raises(ValueError):
+        c({'some': ('a', 'b')})
 
 
 def test_range():
     c = EnsureRange(min=3, max=7)
     # this should always work
-    assert_equal(c(3.0), 3.0)
+    assert c(3.0) == 3.0
 
     # this should always fail
-    assert_raises(ValueError, lambda: c(2.9999999))
-    assert_raises(ValueError, lambda: c(77))
-    assert_raises(TypeError, lambda: c('fail'))
-    assert_raises(TypeError, lambda: c((3, 4)))
+    with pytest.raises(ValueError):
+        c(2.9999999)
+    with pytest.raises(ValueError):
+        c(77)
+    with pytest.raises(TypeError):
+        c('fail')
+    with pytest.raises(TypeError):
+        c((3, 4))
     # since no type checks are performed
-    assert_raises(TypeError, lambda: c('7'))
+    with pytest.raises(TypeError):
+        c('7')
 
     # Range doesn't have to be numeric
     c = EnsureRange(min="e", max="qqq")
-    assert_equal(c('e'), 'e')
-    assert_equal(c('fa'), 'fa')
-    assert_equal(c('qq'), 'qq')
-    assert_raises(ValueError, c, 'a')
-    assert_raises(ValueError, c, 'qqqa')
+    assert c('e') == 'e'
+    assert c('fa') == 'fa'
+    assert c('qq') == 'qq'
+    with pytest.raises(ValueError):
+        c('a')
+    with pytest.raises(ValueError):
+        c('qqqa')
 
 
 def test_listof():
     c = EnsureListOf(str)
-    assert_equal(c(['a', 'b']), ['a', 'b'])
-    assert_equal(c(['a1', 'b2']), ['a1', 'b2'])
+    assert c(['a', 'b']) == ['a', 'b']
+    assert c(['a1', 'b2']) == ['a1', 'b2']
 
 
 def test_tupleof():
     c = EnsureTupleOf(str)
-    assert_equal(c(('a', 'b')), ('a', 'b'))
-    assert_equal(c(('a1', 'b2')), ('a1', 'b2'))
+    assert c(('a', 'b')) == ('a', 'b')
+    assert c(('a1', 'b2')) == ('a1', 'b2')
 
 
 def test_constraints():
     # this should always work
     c = Constraints(EnsureFloat())
-    assert_equal(c(7.0), 7.0)
+    assert c(7.0) == 7.0
     c = Constraints(EnsureFloat(), EnsureRange(min=4.0))
-    assert_equal(c(7.0), 7.0)
+    assert c(7.0) == 7.0
     # __and__ form
     c = EnsureFloat() & EnsureRange(min=4.0)
-    assert_equal(c(7.0), 7.0)
-    assert_raises(ValueError, c, 3.9)
+    assert c(7.0) == 7.0
+    with pytest.raises(ValueError):
+        c(3.9)
     c = Constraints(EnsureFloat(), EnsureRange(min=4), EnsureRange(max=9))
-    assert_equal(c(7.0), 7.0)
-    assert_raises(ValueError, c, 3.9)
-    assert_raises(ValueError, c, 9.01)
+    assert c(7.0) == 7.0
+    with pytest.raises(ValueError):
+        c(3.9)
+    with pytest.raises(ValueError):
+        c(9.01)
     # __and__ form
     c = EnsureFloat() & EnsureRange(min=4) & EnsureRange(max=9)
-    assert_equal(c(7.0), 7.0)
-    assert_raises(ValueError, c, 3.99)
-    assert_raises(ValueError, c, 9.01)
+    assert c(7.0) == 7.0
+    with pytest.raises(ValueError):
+        c(3.99)
+    with pytest.raises(ValueError):
+        c(9.01)
     # and reordering should not have any effect
     c = Constraints(EnsureRange(max=4), EnsureRange(min=9), EnsureFloat())
-    assert_raises(ValueError, c, 3.99)
-    assert_raises(ValueError, c, 9.01)
+    with pytest.raises(ValueError):
+        c(3.99)
+    with pytest.raises(ValueError):
+        c(9.01)
 
 
 def test_altconstraints():
     # this should always work
     c = AltConstraints(EnsureFloat())
-    assert_equal(c(7.0), 7.0)
+    assert c(7.0) == 7.0
     c = AltConstraints(EnsureFloat(), EnsureNone())
-    assert_equal(c.short_description(), '(float or None)')
-    assert_equal(c(7.0), 7.0)
-    assert_equal(c(None), None)
+    assert c.short_description(), '(float or None)'
+    assert c(7.0) == 7.0
+    assert c(None) is None
     # __or__ form
     c = EnsureFloat() | EnsureNone()
-    assert_equal(c(7.0), 7.0)
-    assert_equal(c(None), None)
+    assert c(7.0) == 7.0
+    assert c(None) is None
 
     # this should always fail
     c = Constraints(EnsureRange(min=0, max=4), EnsureRange(min=9, max=11))
-    assert_raises(ValueError, c, 7.0)
+    with pytest.raises(ValueError):
+        c(7.0)
     c = EnsureRange(min=0, max=4) | EnsureRange(min=9, max=11)
-    assert_equal(c(3.0), 3.0)
-    assert_equal(c(9.0), 9.0)
-    assert_raises(ValueError, c, 7.0)
-    assert_raises(ValueError, c, -1.0)
+    assert c(3.0) == 3.0
+    assert c(9.0) == 9.0
+    with pytest.raises(ValueError):
+        c(7.0)
+    with pytest.raises(ValueError):
+        c(-1.0)
 
 
 def test_both():
@@ -226,12 +263,13 @@ def test_both():
             EnsureFloat(),
             EnsureRange(min=7.0, max=44.0)),
         EnsureNone())
-    assert_equal(c(7.0), 7.0)
-    assert_equal(c(None), None)
+    assert c(7.0) == 7.0
+    assert c(None) is None
     # this should always fail
-    assert_raises(ValueError, lambda: c(77.0))
+    with pytest.raises(ValueError):
+        c(77.0)
 
 
 def test_type_str():
-    assert_equal(_type_str((str,)), 'str')
-    assert_equal(_type_str(str), 'str')
+    assert _type_str((str,)) == 'str'
+    assert _type_str(str) == 'str'
