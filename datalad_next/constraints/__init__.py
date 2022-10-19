6a1f565c529fe83547e9034c27a2efec1bde6057
Michael Hanke <michael.hanke@gmail.com>
2022-10-19 19:14:07 +0200

    Expose constraints as top-level

---
+++ datalad_next/constraints/__init__.py
@@ -0,0 +1,19 @@
+
+# expose constraints with direct applicability, but not
+# base and helper classes
+from .basic import (
+    EnsureBool,
+    EnsureCallable,
+    EnsureChoice,
+    EnsureFloat,
+    EnsureInt,
+    EnsureIterableOf,
+    EnsureKeyChoice,
+    EnsureListOf,
+    EnsureMapping,
+    EnsureNone,
+    EnsurePath,
+    EnsureStr,
+    EnsureRange,
+    EnsureTupleOf
+)
