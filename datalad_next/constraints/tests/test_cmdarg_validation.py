ba7bf7b382fe59fc902917f50527d37ad102f4bd
Michael Hanke <michael.hanke@gmail.com>
2022-11-09 20:57:13 +0100

    Add test with a sketch on how to achieve input validation
    The test demos how fairly complex input variety and sources can be
    handled with just constraints, fully abstracting away this can of
    handling from both, the implementation of a specific command (e.g.
    `__call__()`), but also requiring no command specific tailoring
    of the actual validation/conversion handling.
    
    Ping datalad/datalad-next#119

---
+++ datalad_next/constraints/tests/test_cmdarg_validation.py
@@ -0,0 +1,131 @@
+from io import StringIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from unittest.mock import patch
+
+from datalad.interface.base import (
+    Interface,
+)
+from datalad.interface.utils import eval_results
+from datalad.support.param import Parameter
+from datalad.utils import on_windows
+from datalad_next.constraints.base import AltConstraints
+from datalad_next.constraints import (
+    EnsureGeneratorFromFileLike,
+    EnsureJSON,
+    EnsureListOf,
+    EnsureMapping,
+    EnsurePath,
+    EnsureURL,
+)
+
+
+class CmdWithValidation(Interface):
+    # this is of little relevance, no validation configured here
+    _params_ = dict(spec=Parameter(args=('spec',), nargs='+'))
+
+    # TODO reduce code-duplication
+    url_constraint = EnsureURL(required=['scheme'])
+    url2path_constraint = EnsureMapping(
+        key=EnsureURL(required=['scheme']), value=EnsurePath(),
+        delimiter='\t'
+    )
+    spec_item_constraint = url2path_constraint | url_constraint \
+        | (EnsureJSON() & url2path_constraint)
+
+    # this is the key bit: a mapping of parameter names to validators
+    _validators_ = dict(
+        # Must not OR: https://github.com/datalad/datalad/issues/7164
+        #spec=spec_item_constraint | EnsureListOf(spec_item_constraint)# \
+        spec=AltConstraints(
+            EnsureListOf(spec_item_constraint),
+            EnsureGeneratorFromFileLike(spec_item_constraint),
+            spec_item_constraint,
+        ),
+    )
+
+    # validation helper, need not be a classmethod
+    @classmethod
+    def validate_args(cls, **kwargs):
+        validated = {}
+        for argname, arg in kwargs.items():
+            validator = cls._validators_.get(argname, lambda x: x)
+            # TODO option to validate all args despite failure
+            try:
+                validated[argname] = validator(arg)
+            except Exception as e:
+                raise ValueError(
+                    f'Validation of parameter {argname!r} failed') from e
+        return validated
+
+    # command implementation that only validated and returns the outcome
+    @staticmethod
+    @eval_results
+    def __call__(spec):
+        # this would be done by the generic command call handling before the
+        # args even hits this method
+        validated = CmdWithValidation.validate_args(spec=spec)
+        yield dict(
+            action='cmd_with_validation',
+            # list() consumes any potential generator
+            spec=list(validated['spec']),
+            status='ok',
+        )
+
+
+def test_cmd_with_validation():
+    target_urls = ['http://example.com', 'file:///dev/null']
+    target_url_path_maps = [
+        {'http://example.com': Path('some/dir/file')},
+        {'file:///dev/null': Path('/dev/null')},
+    ]
+    json_lines = '{"http://example.com":"some/dir/file"}\n' \
+                 '{"file:///dev/null":"/dev/null"}'
+
+    for input, target in (
+        # perfect input
+        (target_urls, target_urls),
+        (target_url_path_maps, target_url_path_maps),
+        # actual invput conversion
+        ([{'http://example.com': 'some/dir/file'},
+          {'file:///dev/null': '/dev/null'}],
+         target_url_path_maps),
+        # custom mapping syntax
+        (['http://example.com\tsome/dir/file',
+          'file:///dev/null\t/dev/null'],
+         target_url_path_maps),
+        # JSON lines
+        (['{"http://example.com":"some/dir/file"}',
+          '{"file:///dev/null":"/dev/null"}'],
+         target_url_path_maps),
+        # from file with JSON lines
+        (StringIO(json_lines), target_url_path_maps),
+    ):
+        res = CmdWithValidation.__call__(
+            input,
+            return_type='item-or-list', result_renderer='disabled',
+        )
+        assert 'spec' in res
+        assert res['spec'] == target
+
+    # read from file
+    if not on_windows:
+        # on windows the write-rewind-test logic is not possible
+        # (PermissionError) -- too lazy to implement a workaround
+        with NamedTemporaryFile('w+') as f:
+            f.write(json_lines)
+            f.seek(0)
+            res = CmdWithValidation.__call__(
+                f.name,
+                return_type='item-or-list', result_renderer='disabled',
+            )
+            assert res['spec'] == target_url_path_maps
+
+    with patch("sys.stdin", StringIO(json_lines)):
+        res = CmdWithValidation.__call__(
+            '-',
+            return_type='item-or-list', result_renderer='disabled',
+        )
+        assert res['spec'] == target_url_path_maps
+
+
