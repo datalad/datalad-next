67efa740cbd8b7291ea46862938d58c0a0d9c421
Michael Hanke <michael.hanke@gmail.com>
2022-11-29 10:16:04 +0100

    Discontinue distributed `datalad.consts` imports
    Ping #145

--- datalad_next/consts.py
+++ datalad_next/consts.py
@@ -5,3 +5,5 @@ except ImportError:  # pragma: no cover
     from datalad_next.utils import on_windows
     # from PY3.10
     COPY_BUFSIZE = 1024 * 1024 if on_windows else 64 * 1024
+
+from datalad.consts import PRE_INIT_COMMIT_SHA
