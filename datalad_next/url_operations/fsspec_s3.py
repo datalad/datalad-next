468ab658190c985a804264e9a5fb27c25c7fea6d
Michael Hanke <michael.hanke@gmail.com>
2023-01-18 10:31:31 +0100

    Support arbitrary FSSPEC customizations for S3 access
    This includes, but is not limited to, being able to specify
    whether or not anonymous access should be attempted (first).
    
    This change paves the way for endpoint customizations and
    anything else that FSSPEC exposes.
    
    Moreover, when an explicit `credential` identifier is given,
    the boto-based attempt to locate credentials or try
    anonymous access first is skipped, and a credential is looked
    up and provisioned immediately.

--- datalad_next/url_operations/fsspec_s3.py
+++ datalad_next/url_operations/fsspec_s3.py
@@ -16,53 +16,66 @@ from . import (
 )
 
 
-def get_fs(url, target_url, *, cfg, credential) -> Tuple:
+def get_fs(url, target_url, *, cfg, credential, **kwargs) -> Tuple:
+    """Get filesystem from s3:// URL
+
+    If no ``credential`` identifier is provided, a first access attempt is made
+    without provisioning any DataLad-based credentials. This enables the
+    underlying botocore package to look for credentials on its own (which, for
+    example, enables "just-works" when running on AWS EC2 instances), unless
+    anonymous access was requested explicitly. Any ``**kwargs`` are passed
+    to ``fsspec.core.url_to_fs()``.
+
+    If access without explicitly provisioned credentials fails with a
+    permission error, a credential is looked up or prompted for, and a second
+    attempt with non-anonymous access and the queried credential is made.
+    """
     object_url = target_url
     s3bucket_name = urlparse(object_url).netloc
-    # TODO consider `endpoint_url` and maybe other customizations too
-    fs_kwargs = {}
-    try:
-        # we start with no explicit credentials. This will cause boto to
-        # use anonymous or some credentials stored in the config files
-        # it recognizes, or use the respective env vars (incl session tokens).
-        # This approach will yield the highest efficiency across a diverse
-        # set of use cases
-        return _get_fs(url, anon=False, **fs_kwargs)
-    except PermissionError as e:
-        # TODO log this error
-        # access without credential provisioning failed. this could mean
-        # different things:
-        # - credentials are deployed externally via env vars, but anonymous
-        #   access (anon=True) would be needed, because the credentials do
-        #   not match
-        # - credentionals are needed and provisioned, but are wrong
-        pass
-    except NoCredentialsError as e:
-        # TODO log this error
-        # credentials are known to be required needed by not available
-        pass
-    except Exception:
-        # something unexpected, reraise
-        raise UrlOperationsRemoteError(object_url) from e
+
+    # if there are instructions to use an explicit datalad-based credential,
+    # there is no point in trying anon-access or any of boto's internal
+    # mechanism
+    if not credential:
+        try:
+            # we start with no explicit credentials. This will cause boto to
+            # use anonymous or some credentials stored in the config files it
+            # recognizes, or use the respective env vars (incl session tokens).
+            # This approach will yield the highest efficiency across a diverse
+            # set of use cases.
+            # note that if anonymous access is desired (on this first attempt),
+            # it must be enabled by adding `anon=True` to the `kwargs`.
+            return _get_fs(url, **kwargs)
+        except PermissionError as e:
+            # TODO log this error
+            # access without credential provisioning failed. this could mean
+            # different things:
+            # - credentials are deployed externally via env vars, but anonymous
+            #   access (anon=True) would be needed, because the credentials do
+            #   not match
+            # - credentionals are needed and provisioned, but are wrong
+            pass
+        except NoCredentialsError as e:
+            # TODO log this error
+            # credentials are known to be required needed by not available
+            pass
+        except Exception:
+            # something unexpected, reraise
+            raise UrlOperationsRemoteError(object_url) from e
 
     # if we get here, access failed in a credential-related fashion.
     # try to determine credentials for this target bucket.
-    # if there are no credentials available, but credentials are deployed
-    # via env vars, remote the credentials and try again
 
-    # TODO the following could possibly migrate into
-    # get_specialremote_credential_properties()
-    #
     # compose a standard realm identifer
     # TODO recognize alternative endpoints here
     host = 's3.amazonaws.com'
-    # this is the way AWS exposes bucket content via https, but we
-    # stick to s3:// to avoid confusion. Therefore we are also not
-    # adding the bucketname as the first component of the URL path
-    # (where it would be in real s3:// URLs, instead of the host).
-    # Taken together we get a specialization of an S3 realm that is
-    # endpoint/service specific (hence we do not confuse AWS
-    # credentials with those of a private MinIO instance).
+    # this is the way AWS exposes bucket content via https in the "virtualhost"
+    # fashion, but we stick to s3:// to avoid confusion. Therefore we are also
+    # not adding the bucketname as the first component of the URL path (where
+    # it would be in real s3:// URLs, instead of the host).  Taken together we
+    # get a specialization of an S3 realm that is endpoint/service specific
+    # (hence we do not confuse AWS credentials with those of a private MinIO
+    # instance).
     # TODO it is not 100% clear to mih whether a credential would
     # always tend to be for a bucket-wide scope, or whether per-object
     # credentials are a thing
@@ -79,16 +92,17 @@ def get_fs(url, target_url, *, cfg, credential) -> Tuple:
 
     credprops = dict(key=cred['key'], secret=cred['secret'])
     if object_url == url:
-        fs_kwargs.update(**credprops)
+        # s3 is the main target, all args are at the top-level
+        kwargs.update(anon=False, **credprops)
     else:
-        fs_kwargs.update(s3=credprops)
+        # s3 is just one of the filesystems needed in a chain
+        # look for any provided s3 config and update for explicit
+        # credential provision
+        kwargs.update(s3=dict(kwargs.get('s3', {}), anon=False, **credprops))
 
     # now try again, this time with a credential
     try:
-        fs_url_stat = _get_fs(
-            url,
-            **fs_kwargs
-        )
+        fs_url_stat = _get_fs(url, **kwargs)
     except PermissionError as e:
         raise UrlOperationsAuthorizationError(object_url) from e
     except Exception as e:
