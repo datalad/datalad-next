cea6e06cd4c12406af3a864f7e210527eeb12497
Michael Hanke <michael.hanke@gmail.com>
2023-02-24 12:43:48 +0100

    100% Test coverage for the `constraints` module

--- datalad_next/constraints/exceptions.py
+++ datalad_next/constraints/exceptions.py
@@ -121,7 +121,7 @@ class ConstraintErrors(ConstraintError):
     def __repr__(self):
         return '{0}({{{1}}})'.format(
             self.__class__.__name__,
-            ', '.join(f'{k!r}={v!r}' for k, v in self.errors.items()),
+            ', '.join(f'{k!r}: {v!r}' for k, v in self.errors.items()),
         )
 
 
