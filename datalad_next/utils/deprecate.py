37aa46ef4e3db58c6dab562f6fc177b8271ea294
Adina Wagner <adina.wagner@t-online.de>
2023-03-21 14:30:55 +0100

    Simplification

--- datalad_next/utils/deprecate.py
+++ datalad_next/utils/deprecate.py
@@ -21,73 +21,47 @@ def deprecated(msg, version, parameter=None, parameter_choice=None):
       Individual parameter choice of specified parameter being deprecated
     """
 
+    def check_parameters(funcargs):
+        if parameter not in funcargs:
+            return False
+        if parameter_choice is not None:
+            val = funcargs[parameter]
+            if isinstance(val, list):
+                if parameter_choice not in val:
+                    return False
+            elif isinstance(parameter_choice, list):
+                if val not in parameter_choice:
+                    return False
+            else:
+                if val != parameter_choice:
+                    return False
+        return True
+
     base_template = "{func} was deprecated in version {version}. {msg}"
 
-    if parameter is None:
-        # the entire class/function is deprecated
-        def decorator(func):
-            @wraps(func)
-            def func_with_deprecation_warning(*args, **kwargs):
-                fname = (
-                    get_wrapped_class(func)
-                    if func.__name__ == "__call__"
-                    else func.__name__
-                )
-                warnings.warn(
-                    base_template.format(func=fname, version=version, msg=msg),
-                    DeprecationWarning,
-                )
-                return func(*args, **kwargs)
-
-            return func_with_deprecation_warning
-
-        return decorator
-
-    # a single parameter, or parameter choice is deprecated
     def decorator(func):
         @wraps(func)
         def func_with_deprecation_warning(*args, **kwargs):
-            # obtain the signature, and check if there are reasons to warn
-            args_info = inspect.getargvalues(inspect.currentframe())
-            # only keyword arguments can be deprecated
-            funcargs = {
-                arg: args_info.locals["kwargs"].get(arg)
-                for arg in args_info.locals["kwargs"]
-            }
-            # has a deprecated parameter been used?
-            if parameter not in funcargs.keys():
-                # there is nothing to deprecate
-                return func(*args, **kwargs)
-            # has a deprecated parameter choice been used?
-            if parameter_choice is not None:
-                val = funcargs[parameter]
-                if isinstance(val, list):
-                    if parameter_choice not in val:
-                        # there is nothing to deprecate
-                        return func(*args, **kwargs)
-                elif isinstance(parameter_choice, list):
-                    if val not in parameter_choice:
-                        # there is nothing to deprecate
-                        return func(*args, **kwargs)
-                else:
-                    if val != parameter_choice:
-                        # there is nothing to deprecate
-                        return func(*args, **kwargs)
-                template = (
-                    f"The parameter value {parameter_choice} of "
-                    f"parameter {parameter} of " + base_template
-                )
-            else:
-                template = "The {parameter} parameter of " + base_template
             fname = (
-                get_wrapped_class(func)
-                if func.__name__ == "__call__"
-                else func.__name__
-            )
+                    get_wrapped_class(func)
+                    if func.__name__ == "__call__"
+                    else func.__name__
+                    )
+
+            if parameter is None:
+                warning = base_template
+            else:
+                if not check_parameters(kwargs):
+                    return func(*args, **kwargs)
+                if parameter_choice is not None:
+                    warning = (
+                            f"The parameter value {parameter_choice} of "
+                            f"parameter {parameter} of " + base_template
+                    )
+                else:
+                    warning = f"The {parameter} parameter of " + base_template
             warnings.warn(
-                template.format(
-                    parameter=parameter, func=fname, version=version, msg=msg
-                ),
+                warning.format(func=fname, version=version, msg=msg),
                 DeprecationWarning,
             )
             return func(*args, **kwargs)
