ee317f4b3bf3fef72049fca15fb3364061528815
Christian Monch <christian.moench@web.de>
2023-06-06 21:01:18 +0200

    add zipfile archive operations with tests
    This commit adds `ZipArchiveOperations`,
    a class that implements `ArchiveOperations`
    for zip files. It makes use of
    `datalad_next.iter_collections.zipfile.iter_zip`
    to implement iteration.
    
    The commit comes with tests. The test
    code includes a pytest-fixture called `sample_zip`,
    which creates a sample zip-file with known content,
    much like the `sample_tar_xz`-fixture.

---
+++ datalad_next/archive_operations/tests/test_zipfile_archive_operations.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import zipfile
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Generator
+
+import pytest
+
+from datalad_next.iter_collections.utils import FileSystemItemType
+from ..zipfile import ZipArchiveOperations
+
+@dataclass
+class TestArchive:
+    path: Path
+    item_count: int
+    content: bytes
+    target_hash: dict[str, str]
+
+
+@pytest.fixture(scope='session')
+def sample_zip(tmp_path_factory) -> Generator[TestArchive, None, None]:
+    """Create a sample zip file
+
+    Provides a path to a zip with files and directories. Any file content is
+    'zip-123\n'. The associated hashes are:
+
+    md5: d700214df5487801e8ee23d31e60382a
+    sha1: b5dfcec4d1b6166067226fae102f7fbcf6bd1bd4
+
+    Layout::
+
+        test-archive
+        ├── onetwothree.txt
+        └── subdir/
+            └── onetwothree_again.txt
+    """
+    path = tmp_path_factory.mktemp('zipfile') / 'sample.zip'
+    file_content = b'zip-123\n'
+    with zipfile.ZipFile(path, mode='w') as zip_file:
+        zip_file.writestr('test-archive/', '')
+        zip_file.writestr('test-archive/subdir/', '')
+        with zip_file.open('test-archive/onetwothree.txt', mode='w') as fp:
+            fp.write(file_content)
+        with zip_file.open('test-archive/subdir/onetwothree_again.txt', mode='w') as fp:
+            fp.write(file_content)
+
+    yield TestArchive(
+        path=path,
+        item_count=4,
+        content=file_content,
+        target_hash={
+            'SHA1': 'b5dfcec4d1b6166067226fae102f7fbcf6bd1bd4',
+            'md5': 'd700214df5487801e8ee23d31e60382a',
+        }
+    )
+    path.unlink()
+
+
+def test_basics(sample_zip: TestArchive):
+    member_name = 'test-archive/onetwothree.txt'
+    with ZipArchiveOperations(sample_zip.path) as archive_ops:
+        with archive_ops.open(member_name) as member:
+            assert member.read() == sample_zip.content
+
+
+def test_containment(sample_zip: TestArchive):
+    member_name = 'test-archive/onetwothree.txt'
+    archive_ops = ZipArchiveOperations(sample_zip.path)
+    assert member_name in archive_ops
+
+
+def test_iterator(sample_zip: TestArchive):
+    archive_ops = ZipArchiveOperations(sample_zip.path)
+    items = list(archive_ops)
+    assert len(items) == sample_zip.item_count
+    for item in items:
+        item_name = (
+            str(item.name) +
+            '/' if item.type == FileSystemItemType.directory
+            else str(item.name)
+        )
+        assert item_name in archive_ops
+    archive_ops.close()
+
+
+def test_open(sample_zip: TestArchive):
+    archive_ops = ZipArchiveOperations(sample_zip.path)
+    file_pointer = set()
+    for item in list(archive_ops):
+        if item.type == FileSystemItemType.file:
+            with archive_ops.open(str(item.name)) as fp:
+                file_pointer.add(fp)
+                assert fp.read(len(sample_zip.content)) == sample_zip.content
+    for fp in file_pointer:
+        assert fp.closed is True
+    archive_ops.close()
---
+++ datalad_next/archive_operations/zipfile.py
@@ -0,0 +1,109 @@
+"""Archive operation handler for zipfiles"""
+
+from __future__ import annotations
+
+import logging
+import zipfile
+from contextlib import contextmanager
+from pathlib import Path
+from typing import (
+    Any,
+    Generator,
+    IO,
+)
+from zipfile import ZipInfo
+
+from datalad_next.config import ConfigManager
+# TODO we might just want to do it in reverse:
+# move the code of `iter_zip` in here and have it call
+# `ZipArchiveOperations(path).__iter__()` instead.
+# However, the flexibility to have `iter_zip()` behave
+# differently depending on parameters (fp=True/False)
+# is nice, and `__iter__()` only has `self`, such that
+# any customization would need to be infused in the whole
+# class. Potentially cumbersome.
+from datalad_next.iter_collections.zipfile import (
+    ZipfileItem,
+    iter_zip,
+)
+from . import ArchiveOperations
+
+
+lgr = logging.getLogger('datalad.ext.next.archive_operations.zipfile')
+
+
+class ZipArchiveOperations(ArchiveOperations):
+    """
+    """
+    def __init__(self,
+                 location: Path,
+                 *,
+                 cfg: ConfigManager | None = None,
+                 **kwargs):
+        """
+        Parameters
+        ----------
+        location: Path
+          ZIP archive location
+        cfg: ConfigManager, optional
+          A config manager instance that is consulted for any supported
+          configuration items
+        **kwargs: dict
+          Keyword arguments that are passed to zipfile.ZipFile-constructor
+        """
+        super().__init__(location, cfg=cfg)
+
+        self.zipfile_kwargs = kwargs
+        # Consider supporting file-like for `location`,
+        # see zipfile.ZipFile(file_like_object)
+        self._zipfile_path = location
+        self._zipfile: zipfile.ZipFile | None = None
+
+    @property
+    def zipfile(self) -> zipfile.ZipFile:
+        if self._zipfile is None:
+            self._zipfile = zipfile.ZipFile(
+                self._zipfile_path,
+                **self.zipfile_kwargs
+            )
+        return self._zipfile
+
+    def close(self) -> None:
+        if self._zipfile:
+            self._zipfile.close()
+            self._zipfile = None
+
+    @contextmanager
+    def open(self, item: str | ZipInfo, **kwargs) -> IO:
+        """Context manager, returning an open file for a member of the archive.
+
+        The file-like object will be closed when the context-handler
+        exits.
+
+        Parameters:
+        ---------
+        item: str | zipfile.ZipInfo
+          Name or ZipInfo-instance that identifies an item in the zipfile
+        kwargs: dict
+          Keyword arguments that will be used for ZipFile.open()
+
+        Returns
+        -------
+        IO
+          A file-like object to read bytes from the item or to write bytes
+          to the item.
+        """
+        with self.zipfile.open(item, **kwargs) as fp:
+            yield fp
+
+    def __contains__(self, item: Any) -> bool:
+        try:
+            self.zipfile.getinfo(item)
+            return True
+        except KeyError:
+            return False
+
+    def __iter__(self) -> Generator[ZipfileItem, None, None]:
+        # if fp=True is needed, either `iter_zip()` can be used
+        # directly, or `ZipArchiveOperations.open`
+        yield from iter_zip(self._zipfile_path, fp=False)
