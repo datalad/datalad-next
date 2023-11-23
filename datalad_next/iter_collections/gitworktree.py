9468963e978af249ee1063e6166af261bacc6c63
Christian Monch <christian.moench@web.de>
2023-11-23 15:15:11 +0100

    adapt iter_gitworktree code to renamed parameter
    This commit adapts the code of `iter_gitworktree`
    to work with the `sep`-parameter of `itemize`. The
    parameter was renamed from `separator` to `sep`.

--- datalad_next/iter_collections/gitworktree.py
+++ datalad_next/iter_collections/gitworktree.py
@@ -262,7 +262,7 @@ def _git_ls_files(path, *args):
         yield from decode_bytes(
             itemize(
                 r,
-                separator=b'\0',
+                sep=b'\0',
                 keep_ends=False,
             )
         )
