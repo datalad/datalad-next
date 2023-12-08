5a05173114ffbf0290243218aa2ed9c494231426
Christian Monch <christian.moench@web.de>
2023-12-08 14:37:20 +0100

    describe exception handling in iter_subproc docs
    This commit describes the handling of exceptions
    in the docstring of `iter_subproc`. It highlights
    the difference between `StopIteration` and other
    exceptions, in connection with a non-zero subprocess
    return code.

--- datalad_next/runners/iter_subproc.py
+++ datalad_next/runners/iter_subproc.py
@@ -49,9 +49,33 @@ def iter_subproc(
       passing chunks to the process, while the standard error thread
       fetches the error output, and while the main thread iterates over
       the process's output from client code in the context.
-      On context exit, the main thread closes the process's standard output,
-      waits for the standard input thread to exit, waits for the standard
-      error thread to exit, and wait for the process to exit.
+
+      On context exit without an exception, the main thread closes the
+      process's standard output, waits for the standard input thread to exit,
+      waits for the standard error thread to exit, and wait for the process to
+      exit. If the process exited with a non-zero return code, an
+      ``IterableSubprocessError`` is raised, containing the process's return
+      code.
+
+      If the context is exited due to an exception that was raised in the
+      context, the main thread terminates the process via ``Popen.terminate()``,
+      closes the process's standard output, waits for the standard input
+      thread to exit, waits for the standard error thread to exit, and waits
+      for the process to exit. If the exception is not `StopIteration`, the
+      exception will be re-raised. If the exception is `StopIteration`, the
+      main thread will check whether the process exited with a non-zero
+      return code. If it did, an ``IterableSubprocessError`` is raised, else
+      a ``StopIteration`` is raised.
+
+      Note that any exception, except from ``StopIteration``, that is raised
+      in the context will bubble up to the main thread. In this case, the
+      return code of the subprocess will not be checked and no
+      ``IterableSubprocessError`` will be raised if the process exited with a
+      non-zero return code. If you need to check the return code, you should
+      catch any exception that is not ``StopIteration`` within the context.
+      Similarly, if you need to know whether a ``StopIteration`` was raised
+      inside the context, you should catch it within the context.
+
     """
     return iterable_subprocess(
         args,
