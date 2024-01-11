cee9c8e2565555966e2b32c3ffb0eb39a4a7b86a
Christian Monch <christian.moench@web.de>
2024-01-11 15:45:54 +0100

    add `returncode` alias to `CommandError`
    This commit adds the alias `returncode` for the
    `code`-attribute in `CommandError`. This allows
    to set and get the return code.

--- datalad_next/patches/commanderror.py
+++ datalad_next/patches/commanderror.py
@@ -1,9 +1,16 @@
-"""Improve ``CommandError`` rendering
+"""Improve ``CommandError`` rendering and add ``returncode`` alias for ``code``
 
-Without this patch that overwrites ``__repr__``, it would use
+
+This patch does two things:
+
+It overwrites ``__repr__``, otherwise ``CommandError` would use
 ``RuntimeError``'s variant and ignore all additional structured information
 except for ``.msg`` -- which is frequently empty and confuses with a
 `CommandError('')` display.
+
+It adds a ``returncode`` alias for ``code``. This unifies return code access
+between ``CommandError`` and `Popen``-like objects, which usually have a
+``returncode`` attribute.
 """
 
 from datalad.runner.exception import CommandError
@@ -14,3 +21,22 @@ def commanderror_repr(self) -> str:
 
 
 CommandError.__repr__ = commanderror_repr
+
+
+# Basic alias idea taken from here:
+# <https://stackoverflow.com/questions/4017572/how-can-i-make-an-alias-to-a-non-function-member-attribute-in-a-python-class>
+_commanderror_aliases = {
+    'returncode': 'code',
+}
+
+def commanderror_getattr(self, item):
+    return object.__getattribute__(self, _commanderror_aliases.get(item, item))
+
+def commanderror_setattr(self, key, value):
+    if key == '_aliases':
+        raise AttributeError('Cannot set `_aliases`')
+    return object.__setattr__(self, _commanderror_aliases.get(key, key), value)
+
+
+CommandError.__getattr__ = commanderror_getattr
+CommandError.__setattr__ = commanderror_setattr
