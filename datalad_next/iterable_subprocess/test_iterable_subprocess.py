d1fc39f26d15294965a533f0f4ea2b0cb9c620bd
Christian Mönch <christian.moench@web.de>
2024-02-02 12:28:17 +0100

    Update datalad_next/iterable_subprocess/test_iterable_subprocess.py
    Co-authored-by: Michael Hanke <michael.hanke@gmail.com>

--- datalad_next/iterable_subprocess/test_iterable_subprocess.py
+++ datalad_next/iterable_subprocess/test_iterable_subprocess.py
@@ -337,4 +337,11 @@ def test_returncode_available_from_generator_with_exception():
         with iterable_subprocess(['echo', 'a'], ()) as echo:
             while True:
                 next(echo)
+    # On a Linux system, all exceptions that are raised before the subprocess
+    # exited will lead to a -15 return code. If StopIteration is raised, the
+    # subprocess will either have terminated which results in a 0-return code,
+    # or the subprocess is still running and will therefore be terminated which
+    # results in a -15 return code. Any other exception than StopIteration,
+    # e.g. a CommandError because echo could not be found, would lead to an
+    # early test-exit and not proceed to the assign-statement.
     assert echo.returncode in (0, -15)
