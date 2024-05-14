dcc655781887ef41b4d98a052fb1970411301307
Christian Monch <christian.moench@web.de>
2024-05-14 10:53:20 +0200

    run all URL-canonification tests on all platforms
    This commit ensures that Windows and non-Windows
    URL-canonification code is run on all platforms.
    This is possible because the code does not depend
    on platform-specific libraries.

--- datalad_next/patches/tests/test_replace_ora_remote.py
+++ datalad_next/patches/tests/test_replace_ora_remote.py
@@ -6,30 +6,43 @@ from ..replace_ora_remote import (
     canonify_url,
     de_canonify_url,
 )
-from datalad_next.utils import on_windows
 
 
 @pytest.mark.parametrize("scheme", ['ria+file', 'file'])
-def test_canonify(scheme):
+def test_canonify(scheme, monkeypatch):
     url_uncanonified = scheme + '://C:/a/b/c'
     url_canonified = scheme + ':///C:/a/b/c'
 
-    if on_windows:
-        assert canonify_url(url_canonified) == url_canonified
-        assert canonify_url(url_uncanonified) == url_canonified
-    else:
-        assert canonify_url(url_canonified) == url_canonified
-        assert canonify_url(url_uncanonified) == url_uncanonified
+    monkeypatch.setattr(
+        'datalad_next.patches.replace_ora_remote.on_windows',
+        True,
+    )
+    assert canonify_url(url_canonified) == url_canonified
+    assert canonify_url(url_uncanonified) == url_canonified
+
+    monkeypatch.setattr(
+        'datalad_next.patches.replace_ora_remote.on_windows',
+        False,
+    )
+    assert canonify_url(url_canonified) == url_canonified
+    assert canonify_url(url_uncanonified) == url_uncanonified
 
 
 @pytest.mark.parametrize("scheme", ['ria+file', 'file'])
-def test_de_canonify(scheme):
+def test_de_canonify(scheme, monkeypatch):
     url_uncanonified = scheme + '://C:/a/b/c'
     url_canonified = scheme + ':///C:/a/b/c'
 
-    if on_windows:
-        assert de_canonify_url(url_canonified) == url_uncanonified
-        assert de_canonify_url(url_uncanonified) == url_uncanonified
-    else:
-        assert de_canonify_url(url_canonified) == url_canonified
-        assert de_canonify_url(url_uncanonified) == url_uncanonified
+    monkeypatch.setattr(
+        'datalad_next.patches.replace_ora_remote.on_windows',
+        True,
+    )
+    assert de_canonify_url(url_canonified) == url_uncanonified
+    assert de_canonify_url(url_uncanonified) == url_uncanonified
+
+    monkeypatch.setattr(
+        'datalad_next.patches.replace_ora_remote.on_windows',
+        False,
+    )
+    assert de_canonify_url(url_canonified) == url_canonified
+    assert de_canonify_url(url_uncanonified) == url_uncanonified
