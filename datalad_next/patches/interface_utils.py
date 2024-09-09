83b7e4e95f0bed02f774db211fbd29513a9979ca
Michael Hanke <michael.hanke@gmail.com>
2024-09-09 18:13:29 +0200

    rf: adjust patch to import and patch `eval_results` directly
    Previously the entrypoint for the patch was `_execute_command_()`. But
    now we aim to refactor the whole thing.

--- datalad_next/patches/interface_utils.py
+++ datalad_next/patches/interface_utils.py
@@ -9,6 +9,8 @@ For details on implementing validation for individual commands see
 :class:`datalad_next.commands.ValidatedInterface`.
 """
 
+from functools import wraps
+import inspect
 import logging
 from typing import (
     Callable,
@@ -22,6 +24,7 @@ from datalad.core.local.resulthooks import (
     match_jsonhook2result,
     run_jsonhook,
 )
+from datalad.interface.common_opts import eval_params
 from datalad.interface.results import known_result_xfms
 from datalad.interface.utils import (
     anInterface,
@@ -32,11 +35,118 @@ from datalad.interface.utils import (
     _process_results,
 )
 from datalad_next.exceptions import IncompleteResultsError
+from datalad.utils import get_wrapped_class
 from . import apply_patch
 from datalad_next.constraints import DatasetParameter
 
 # use same logger as -core
 lgr = logging.getLogger('datalad.interface.utils')
+
+
+# This function interface is taken from
+# datalad-core@982cca549ae29a1c86a0d6736bc3d6dfec370433
+def eval_results(wrapped):
+    """Decorator for return value evaluation of datalad commands.
+
+    Note, this decorator is only compatible with commands that return
+    status dict sequences!
+
+    Two basic modes of operation are supported: 1) "generator mode" that
+    `yields` individual results, and 2) "list mode" that returns a sequence of
+    results. The behavior can be selected via the kwarg `return_type`.
+    Default is "list mode".
+
+    This decorator implements common functionality for result rendering/output,
+    error detection/handling, and logging.
+
+    Result rendering/output configured via the `result_renderer` keyword
+    argument of each decorated command. Supported modes are: 'generic' (a
+    generic renderer producing one line per result with key info like action,
+    status, path, and an optional message); 'json' (a complete JSON line
+    serialization of the full result record), 'json_pp' (like 'json', but
+    pretty-printed spanning multiple lines), 'tailored' custom output
+    formatting provided by each command class (if any), or 'disabled' for
+    no result rendering.
+
+    Error detection works by inspecting the `status` item of all result
+    dictionaries. Any occurrence of a status other than 'ok' or 'notneeded'
+    will cause an IncompleteResultsError exception to be raised that carries
+    the failed actions' status dictionaries in its `failed` attribute.
+
+    Status messages will be logged automatically, by default the following
+    association of result status and log channel will be used: 'ok' (debug),
+    'notneeded' (debug), 'impossible' (warning), 'error' (error).  Logger
+    instances included in the results are used to capture the origin of a
+    status report.
+
+    Parameters
+    ----------
+    func: function
+      __call__ method of a subclass of Interface,
+      i.e. a datalad command definition
+    """
+
+    @wraps(wrapped)
+    def eval_func(*args, **kwargs):
+        lgr.log(2, "Entered eval_func for %s", wrapped)
+        # determine the command class associated with `wrapped`
+        wrapped_class = get_wrapped_class(wrapped)
+
+        # retrieve common options from kwargs, and fall back on the command
+        # class attributes, or general defaults if needed
+        kwargs = kwargs.copy()  # we will pop, which might cause side-effect
+        common_params = {
+            p_name: kwargs.pop(
+                # go with any explicitly given default
+                p_name,
+                # otherwise determine the command class and pull any
+                # default set in that class
+                getattr(wrapped_class, p_name))
+            for p_name in eval_params}
+
+        # short cuts and configured setup for common options
+        return_type = common_params['return_type']
+
+        if return_type == 'generator':
+            # hand over the generator
+            lgr.log(2,
+                    "Returning generator_func from eval_func for %s",
+                    wrapped_class)
+            return _execute_command_(
+                interface=wrapped_class,
+                cmd=wrapped,
+                cmd_args=args,
+                cmd_kwargs=kwargs,
+                exec_kwargs=common_params,
+            )
+        else:
+            @wraps(_execute_command_)
+            def return_func(*args_, **kwargs_):
+                results = _execute_command_(
+                    interface=wrapped_class,
+                    cmd=wrapped,
+                    cmd_args=args,
+                    cmd_kwargs=kwargs,
+                    exec_kwargs=common_params,
+                )
+                if inspect.isgenerator(results):
+                    # unwind generator if there is one, this actually runs
+                    # any processing
+                    results = list(results)
+                if return_type == 'item-or-list' and \
+                        len(results) < 2:
+                    return results[0] if results else None
+                else:
+                    return results
+
+            lgr.log(2,
+                    "Returning return_func from eval_func for %s",
+                    wrapped_class)
+            return return_func(*args, **kwargs)
+
+    ret = eval_func
+    ret._eval_results = True
+    return ret
 
 
 # this is a replacement for datalad.interface.base.get_allargs_as_kwargs
@@ -291,6 +401,9 @@ def _execute_command_(
 
 # apply patch
 patch_msg = \
-    'Apply datalad-next patch to interface.(utils|base).py:_execute_command_'
-apply_patch('datalad.interface.base', None, '_execute_command_',
-            _execute_command_, msg=patch_msg)
+    'Apply datalad-next patch to interface.(utils|base).py:eval_results'
+apply_patch('datalad.interface.base', None, 'eval_results',
+            eval_results, msg=patch_msg)
+# we need to patch the datalad-next import location too
+apply_patch('datalad_next.commands', None, 'eval_results',
+            eval_results, msg=patch_msg)
